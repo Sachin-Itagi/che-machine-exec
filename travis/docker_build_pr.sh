a9e12c4765815b875587173ae359f87cfa5c526c
Sachin <sachin_itagi@persistent.co.in>
2021-05-20 13:49:28 +0530

    Travis CI Update
    Run Certain Stages on Push

--- travis/docker_build_pr.sh
+++ travis/docker_build_pr.sh
@@ -2,4 +2,3 @@ echo "Quay Login"
 docker login quay.io -u=$QUAY_USERNAME -p=$QUAY_PASSWORD
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker manifest create quay.io/sachin_itagi/che-machine-exec:pr-check --amend quay.io/sachin_itagi/che-machine-exec:manifest-amd64 --amend quay.io/sachin_itagi/che-machine-exec:manifest-ppc64le
-docker manifest push quay.io/sachin_itagi/che-machine-exec:pr-check
